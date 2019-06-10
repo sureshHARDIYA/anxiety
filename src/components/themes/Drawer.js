013b32b947ed7bddee71b6c729c47234054be3f5
Hung(Jim) <hunghk.it@gmail.com>
2019-06-11 00:37:50 +0700

    add icons in sidebar menu

--- src/components/themes/Drawer.js
+++ src/components/themes/Drawer.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { NavigationActions } from 'react-navigation';
-
 import * as AppAction from '@src/actions/app';
 import * as AppSelect from '@src/selectors/app';
 import { strings } from '@src/i18n';
 import { View, Image, Text, TouchableHighlight, StyleSheet } from 'react-native';
-import { Drawer } from '@ant-design/react-native';
+import { Drawer, Icon } from '@ant-design/react-native';
 import { Colors, Config } from '@src/constants';
 
 const DrawerMenu = ({ children, style, open, onDrawer, onNavigation }) => {
@@ -22,19 +21,46 @@ const DrawerMenu = ({ children, style, open, onDrawer, onNavigation }) => {
           style={Style.item}
           onPress={() => onNavigation('Home')}
         >
-          <Text style={Style.itemText}>{strings('menu.home')}</Text>
+          <View style={Style.center}>
+            <Icon color={Colors.white} name="home" />
+            <Text style={Style.itemText}> {strings('menu.home')}</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={Style.item}
+          onPress={() => onNavigation('History')}
+        >
+          <View style={Style.center}>
+            <Icon color={Colors.white} name="clock-circle" />
+            <Text style={Style.itemText}> {strings('menu.history')}</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={Style.item}
+          onPress={() => onNavigation('MCT')}
+        >
+          <View style={Style.center}>
+            <Icon color={Colors.white} name="star" />
+            <Text style={Style.itemText}> {strings('menu.mct')}</Text>
+          </View>
         </TouchableHighlight>
         <TouchableHighlight
           style={Style.item}
           onPress={() => onNavigation('Info')}
         >
-          <Text style={Style.itemText}>{strings('menu.info')}</Text>
+          <View style={Style.center}>
+            <Icon color={Colors.white} name="container" />
+            <Text style={Style.itemText}> {strings('menu.info')}</Text>
+          </View>
         </TouchableHighlight>
         <TouchableHighlight
           style={Style.item}
           onPress={() => onNavigation('Setting')}
         >
-          <Text style={Style.itemText}>{strings('menu.settings')}</Text>
+          <View style={Style.center}>
+            <Icon color={Colors.white} name="setting" />
+            <Text style={Style.itemText}> {strings('menu.settings')}</Text>
+          </View>
         </TouchableHighlight>
       </View>
     </View>
@@ -82,8 +108,14 @@ const Style = StyleSheet.create({
     backgroundColor: Colors.transparent,
   },
   itemText: {
-    fontWeight: '600',
+    marginLeft: 10,
+    fontWeight: 'bold',
+    alignItems: 'center',
     color: Colors.white,
+  },
+  center: {
+    flexDirection: 'row',
+    alignItems: 'center',
   }
 });
 
