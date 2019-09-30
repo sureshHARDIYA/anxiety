3ee22ee597501dcad62e0e148f687771300d14cc
Hung(Jim) <hunghk.it@gmail.com>
2019-09-30 20:16:59 +0700

    Remove arm64 v8a bit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { AppRegistry } from 'react-native';
 import crashlytics from 'react-native-fabric-crashlytics';
-import App from './src';
-import { name as appName } from './app.json';
 
 crashlytics.init();
+
+import App from './src'; // eslint-disable-line
+import { name as appName } from './app.json'; // eslint-disable-line
+
 
 AppRegistry.registerComponent(appName, () => App);
